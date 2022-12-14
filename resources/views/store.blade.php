5a010a95263d1c767a08bf43e1623eb9fe6d2cd9
luishs0 <luishs.doc@gmail.com>
2022-12-14 15:26:27 +0100

    bonus

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -5,9 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel First</title>
+    <title>Store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
 </head>
 
 <body>
@@ -22,10 +21,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                        <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('store') }}">Store</a>
+                        <a class="nav-link active" href="{{ route('store') }}">Store</a>
                     </li>
                 </ul>
             </div>
@@ -33,14 +32,7 @@
     </nav>
     <!-- /NAVBAR -->
 
-    <h1>{{ $sayHello }} World</h1>
-
-    <h4>FrameWorks:</h4>
-    <ul>
-        @foreach ($frameWorks as $frameWork)
-        <li>{{ $frameWork }}</li>
-        @endforeach
-    </ul>
+    <h1>{{ $store }}</h1>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     $sayHello = "Hello";
@@ -26,5 +16,13 @@ Route::get('/', function () {
         ]
     ];
 
-    return view('Home', $data);
-});
+    return view('home', $data);
+})->name("home");
+
+
+
+Route::get('/store', function () {
+    $store = "Store";
+
+    return view('store', compact("store"));
+})->name("store");
